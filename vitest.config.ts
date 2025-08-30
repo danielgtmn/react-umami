4e95e65d10f4535f8714a0841e0e791034d3760b
Daniel Gietmann <dgietmann@proton.me>
2025-08-30 21:18:22 +0200

    fix: add @vitest/coverage-v8

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,5 +8,17 @@ export default defineConfig({
     environment: 'jsdom',
     globals: true,
     setupFiles: ['./src/test/setup.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/test/',
+        '**/*.d.ts',
+        'dist/',
+        '.husky/',
+        '.github/',
+      ],
+    },
   },
 });
